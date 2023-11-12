5a85539298b079fb8b5063e33d8ef0217030677b
saci zakaria <sacideveloppeur@gmail.com>
2023-11-12 13:21:07 +0100

    $ vagrant init  ubuntu/jammy64

---
+++ readme.cmd
@@ -0,0 +1,7 @@
+# Command Linux
+sacid@Gen□ve MINGW64 /d/vagrant-vms/ubuntu
+$ vagrant init  ubuntu/jammy64
+A `Vagrantfile` has been placed in this directory. You are now
+ready to `vagrant up` your first virtual environment! Please read
+the comments in the Vagrantfile as well as documentation on
+`vagrantup.com` for more information on using Vagrant.

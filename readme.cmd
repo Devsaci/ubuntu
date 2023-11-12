34aafb4b656282d1e1c58841624784ab4b308f4b
saci zakaria <sacideveloppeur@gmail.com>
2023-11-12 13:24:29 +0100

    $ vagrant up

--- readme.cmd
+++ readme.cmd
@@ -5,3 +5,6 @@ A `Vagrantfile` has been placed in this directory. You are now
 ready to `vagrant up` your first virtual environment! Please read
 the comments in the Vagrantfile as well as documentation on
 `vagrantup.com` for more information on using Vagrant.
+
+sacid@Gen□ve MINGW64 /d/vagrant-vms/ubuntu (main)
+$ vagrant up
